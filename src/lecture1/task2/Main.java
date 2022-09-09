41fc2d3ab66a5cc253aa686d40e51b59a802bf49
ssmirn0v <31247885+ssmirn0v@users.noreply.github.com>
2022-09-09 16:17:10 +0300

    changes according to code conventions

--- src/lecture1/task2/Main.java
+++ src/lecture1/task2/Main.java
@@ -30,7 +30,9 @@ public class Main {
     }
 
     public static void qs(Comparable[] arr, int lo, int hi) {
-        if (hi <= lo) return;
+        if (hi <= lo) {
+            return;
+        }
 
         int j = partition(arr, lo, hi);
         qs(arr, lo, j-1);
@@ -43,9 +45,19 @@ public class Main {
 
         Comparable v = arr[lo];
         while (true) {
-            while (arr[++i].compareTo(v) < 0) if (i == hi) break;
-            while (arr[--j].compareTo(v) > 0) if (j == lo) break;
-            if (i >= j) break;
+            while (arr[++i].compareTo(v) < 0) {
+                if (i == hi) {
+                    break;
+                }
+            }
+            while (arr[--j].compareTo(v) > 0) {
+                if (j == lo) {
+                    break;
+                }
+            }
+            if (i >= j) {
+                break;
+            }
 
             exch(arr, i, j);
         }
